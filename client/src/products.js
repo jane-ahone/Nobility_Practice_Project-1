30139dbb865e7f6a80ebddaa3c97bbc49d79946b
rachmi00 <r.agyingi@gmail.com>
2024-08-11 23:27:07 +0100

    Here is the basic file structure template to build the front end from, please modify it oo

---
+++ client/src/products.js
@@ -0,0 +1,87 @@
+import bag1 from "./assets/Pics and icons/bag1.svg"
+import bag2 from "./assets/Pics and icons/bag2.png"
+import bag3 from "./assets/Pics and icons/bag3.png"
+import bag4 from "./assets/Pics and icons/bag4.png"
+import shoe1 from "./assets/Pics and icons/shoe1.svg"
+import shoe2 from "./assets/Pics and icons/shoe2.png"
+import shoe3 from "./assets/Pics and icons/shoe3.png"
+import shoe4 from "./assets/Pics and icons/shoe4.png"
+import shoe5 from "./assets/Pics and icons/shoe5.png"
+import shoe6 from "./assets/Pics and icons/shoe6.png"
+import shoe7 from "./assets/Pics and icons/shoe7.png"
+
+export const PRODUCTS = {
+    "categories": ["Dress", "Shirt", "Man Work Equipment", "Woman Bag"],
+    "products": [
+      {
+        "id": 1,
+        "name": "FS - Nike Air Max 270 React...",
+        "category": "Shoes",
+        "saleType": "Flash Sale",
+        "price": 299.43,
+        "originalPrice": 534.33,
+        "discount": 24,
+        "image": bag1,
+        "reviews": [
+          {
+            "rating": 4,
+            "comment": "Great product!",
+            "photos": ["path/to/review-photo1.jpg", "path/to/review-photo2.jpg"]
+          }
+        ]
+      },
+      {
+        "id": 1,
+        "name": "FS - Nike Air Max 270 React...",
+        "category": "Shoes",
+        "saleType": "Flash Sale",
+        "price": 299.43,
+        "originalPrice": 534.33,
+        "discount": 24,
+        "image": bag1,
+        "reviews": [
+          {
+            "rating": 4,
+            "comment": "Great product!",
+            "photos": ["path/to/review-photo1.jpg", "path/to/review-photo2.jpg"]
+          }
+        ]
+      },
+      {
+        "id": 1,
+        "name": "FS - Nike Air Max 270 React...",
+        "category": "Shoes",
+        "saleType": "Flash Sale",
+        "price": 299.43,
+        "originalPrice": 534.33,
+        "discount": 24,
+        "image": bag1,
+        "reviews": [
+          {
+            "rating": 4,
+            "comment": "Great product!",
+            "photos": ["path/to/review-photo1.jpg", "path/to/review-photo2.jpg"]
+          }
+        ]
+      },
+      {
+        "id": 1,
+        "name": "FS - Nike Air Max 270 React...",
+        "category": "Shoes",
+        "saleType": "Flash Sale",
+        "price": 299.43,
+        "originalPrice": 534.33,
+        "discount": 24,
+        "image": bag2,
+        "reviews": [
+          {
+            "rating": 4,
+            "comment": "Great product!",
+            "photos": ["path/to/review-photo1.jpg", "path/to/review-photo2.jpg"]
+          }
+        ]
+      },
+    
+    ]
+  }
+  
