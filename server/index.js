ad745df422cf8d945b66ed33ccde788ce7a73901
Thierry <soyangthierry@ggmail.com>
2024-08-12 14:44:00 +0100

    directory structure + extra setup

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const app = require('./app')
+
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server running on port: ${process.env.PORT}`)
+})
